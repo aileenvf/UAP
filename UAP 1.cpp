a6b4cad2b14fb60cd39fec1fa50e99415816d4da
aileenvf <146709308+aileenvf@users.noreply.github.com>
2024-01-12 15:08:22 +0700

    Add files via upload

---
+++ UAP 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int baris, spasi;
+	cout << Masukkan Input; ";
+	cin >> baris;
+
+	//
+	for (int i = 1; j = 0; < baris; ++i, j=10)
+	{
+		for (baris = 1; baris <= spasi - i; ++baris)
+		{
+			cout << "  ";
+		}
+	}
+	while (j != 2 * i - 1)
+	{
+		cout << "* ";
+		++j;
+	}
+	cout << endl;
+
+return 0;
+}
